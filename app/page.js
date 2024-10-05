3689393a97be7b8f795a736fed064880981974da
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-10-05 18:36:06 +0200

    hero section content and responsive img

--- app/page.js
+++ app/page.js
@@ -1,7 +1,47 @@
 "use client";
 
-import { Wrapper } from "./page.styled";
+import Image from "next/image";
+import {
+  HeroWrapper,
+  MainPageImgWrapper,
+  HeroContentBox,
+  HeroHeading,
+  HeroText,
+} from "./page.styled";
 
 export default function Home() {
-  return <Wrapper>Hello Home Page</Wrapper>;
+  return (
+    <>
+      <HeroWrapper>
+        <MainPageImgWrapper>
+          <source
+            srcSet="/assets/home/desktop/image-hero-paramour.jpg"
+            media="(min-width: 62em)"
+          />
+          <source
+            srcSet="/assets/home/tablet/image-hero-paramour.jpg"
+            media="(min-width: 34.5em)"
+          />
+          <source
+            srcSet="/assets/home/mobile/image-hero-paramour.jpg"
+            media="(min-width: 15em)"
+          />
+          <Image
+            src="/assets/home/mobile/image-hero-paramour.jpg"
+            alt="hero image"
+            width={300}
+            height={560}
+            layout="responsive"
+          />
+        </MainPageImgWrapper>
+        <HeroContentBox>
+          <HeroHeading>Project Paramour</HeroHeading>
+          <HeroText>
+            Project made for an art museum near Southwest London. Project
+            Paramour is a statement of bold, modern architecture.
+          </HeroText>
+        </HeroContentBox>
+      </HeroWrapper>
+    </>
+  );
 }
--- app/page.styled.js
+++ app/page.styled.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
-  background-color: pink;
+export const HeroWrapper = styled.div``;
+
+export const MainPageImgWrapper = styled.picture`
+  filter: brightness(0.8);
 `;
+
+export const HeroContentBox = styled.div``;
+
+export const HeroHeading = styled.h1``;
+
+export const HeroText = styled.p``;
