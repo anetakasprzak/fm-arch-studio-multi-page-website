f5160739c1d364df4d3f88dd19ec56363480ffa7
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-10-05 09:43:35 +0200

    creating data for portfolio

---
+++ app/data/index.js
@@ -0,0 +1,122 @@
+export const portfolioData = [
+  {
+    id: 1,
+    title: "Seraph Station",
+    date: "September 2019",
+    image: {
+      mobile: "/assets/portfolio/mobile/image-seraph.jpg",
+      tablet: "/assets/portfolio/tablet/image-seraph.jpg",
+      desktop: "/assets/portfolio/desktop/image-seraph.jpg",
+    },
+  },
+  {
+    id: 2,
+    title: "Eebox Building",
+    date: "August 2017",
+    image: {
+      mobile: "/assets/portfolio/mobile/image-eebox.jpg",
+      tablet: "/assets/portfolio/tablet/image-eebox.jpg",
+      desktop: "/assets/portfolio/desktop/image-eebox.jpg",
+    },
+  },
+  {
+    id: 3,
+    title: "Federal II Tower",
+    date: "March 2017",
+    image: {
+      mobile: "/assets/portfolio/mobile/image-federal.jpg",
+      tablet: "/assets/portfolio/tablet/image-federal.jpg",
+      desktop: "/assets/portfolio/desktop/image-federal.jpg",
+    },
+  },
+  {
+    id: 4,
+    title: "Project Del Sol",
+    date: "January 2016",
+    image: {
+      mobile: "/assets/portfolio/mobile/image-del-sol.jpg",
+      tablet: "/assets/portfolio/tablet/image-del-sol.jpg",
+      desktop: "/assets/portfolio/desktop/image-del-sol.jpg",
+    },
+  },
+  {
+    id: 5,
+    title: "Le Prototype",
+    date: "October 2015",
+    image: {
+      mobile: "/assets/portfolio/mobile/image-prototype.jpg",
+      tablet: "/assets/portfolio/tablet/image-prototype.jpg",
+      desktop: "/assets/portfolio/desktop/image-prototype.jpg",
+    },
+  },
+  {
+    id: 6,
+    title: "228B Tower",
+    date: "April 2015",
+    image: {
+      mobile: "/assets/portfolio/mobile/image-228b.jpg",
+      tablet: "/assets/portfolio/tablet/image-228b.jpg",
+      desktop: "/assets/portfolio/desktop/image-228b.jpg",
+    },
+  },
+  {
+    id: 7,
+    title: "Grand Edelweiss Hotel",
+    date: "December 2013",
+    image: {
+      mobile: "/assets/portfolio/mobile/image-edelweiss.jpg",
+      tablet: "/assets/portfolio/tablet/image-edelweiss.jpg",
+      desktop: "/assets/portfolio/desktop/image-edelweiss.jpg",
+    },
+  },
+  {
+    id: 8,
+    title: "Netcry Tower",
+    date: "August 2012",
+    image: {
+      mobile: "/assets/portfolio/mobile/image-netcry.jpg",
+      tablet: "/assets/portfolio/tablet/image-netcry.jpg",
+      desktop: "/assets/portfolio/desktop/image-netcry.jpg",
+    },
+  },
+  {
+    id: 9,
+    title: "Hypers",
+    date: "January 2012",
+    image: {
+      mobile: "/assets/portfolio/mobile/image-hypers.jpg",
+      tablet: "/assets/portfolio/tablet/image-hypers.jp",
+      desktop: "/assets/portfolio/desktop/image-hypers.jp",
+    },
+  },
+  {
+    id: 10,
+    title: "SXIV Tower",
+    date: "March 2011",
+    image: {
+      mobile: "/assets/portfolio/mobile/image-sxiv.jpg",
+      tablet: "/assets/portfolio/tablet/image-sxiv.jpg",
+      desktop: "/assets/portfolio/desktop/image-sxiv.jpg",
+    },
+  },
+  {
+    id: 11,
+    title: "Trinity Bank Tower",
+    date: "September 2010",
+    image: {
+      mobile: "/assets/portfolio/mobile/image-trinity.jpg",
+      tablet: "/assets/portfolio/tablet/image-trinity.jpg",
+      desktop: "/assets/portfolio/desktop/image-trinity.jpg",
+    },
+  },
+  {
+    id: 12,
+    title: "Project Paramour",
+    date: "February 2008",
+    image: {
+      mobile: "/assets/portfolio/mobile/image-paramour.jpg",
+      tablet: "/assets/portfolio/tablet/image-paramour.jpg",
+      desktop: "/assets/portfolio/desktop/image-paramour.jpg",
+    },
+  },
+];
