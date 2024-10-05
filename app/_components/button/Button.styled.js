f82f4a00e04e08eda9bb2faecb5c5825498f7b1a
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-10-05 19:32:50 +0200

    button hover and media query

--- app/_components/button/Button.styled.js
+++ app/_components/button/Button.styled.js
@@ -3,10 +3,24 @@ import styled from "styled-components";
 export const ButtonBox = styled.button`
   display: flex;
   align-items: center;
+  width: auto;
   color: #fff;
   background-color: #1b1d23;
-  padding: 2.5rem;
-  font-size: 1.8rem;
+  padding: 2rem;
+  border: none;
+  font-size: 1.6rem;
   font-weight: 700;
-  gap: 2.4rem;
+  gap: 2rem;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  @media screen and (min-width: 28.5em) {
+    padding: 2.5rem;
+    font-size: 1.8rem;
+    gap: 2.4rem;
+  }
+
+  &:hover {
+    background-color: #60636d;
+  }
 `;
