42bcc303de8d59a7232eb356fa01009b0ca029ca
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-10-10 18:15:23 +0200

    fix styles

--- app/(root)/about/page.js
+++ app/(root)/about/page.js
@@ -1,7 +1,28 @@
+"use client";
+
 import "./page.styled";
+import { AboutHeroImageWrapper, AboutHeroWrapper } from "./page.styled";
 
 const AboutPage = () => {
-  return <div>About page</div>;
+  return (
+    <AboutHeroWrapper>
+      <AboutHeroImageWrapper>
+        <source
+          srcSet="/assets/about/desktop/image-hero.jpg"
+          media="(min-width: 48em)"
+        />
+        <source
+          srcSet="/assets/about/tablet/image-hero.jpg"
+          media="(min-width: 34.5em)"
+        />
+        <source
+          srcSet="/assets/about/mobile/image-hero.jpg"
+          media="(min-width: 15em)"
+        />
+        <img src="/assets/about/mobile/image-hero.jpg" alt="hero image" />
+      </AboutHeroImageWrapper>
+    </AboutHeroWrapper>
+  );
 };
 
 export default AboutPage;
---
+++ app/(root)/about/page.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const AboutHeroWrapper = styled.div``;
+
+export const AboutHeroImageWrapper = styled.picture`
+  filter: brightness(0.7);
+`;
