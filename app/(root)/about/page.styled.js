a82822267110cb14dbe45429dc3148c0756f9369
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-10-10 20:24:02 +0200

    about page hero for tablet and desktop

--- app/(root)/about/page.styled.js
+++ app/(root)/about/page.styled.js
@@ -3,6 +3,15 @@ import styled from "styled-components";
 export const AboutHeroWrapper = styled.div`
   display: grid;
   place-items: center;
+
+  @media screen and (min-width: 62em) {
+    place-items: start;
+    padding: 0 7rem;
+  }
+
+  @media screen and (min-width: 90em) {
+    padding: 0 12rem;
+  }
 `;
 
 export const AboutHeroImageWrapper = styled.picture`
@@ -31,7 +40,41 @@ export const HeroTextBox = styled.div`
     place-self: end;
     margin-top: -35rem;
     position: relative;
-    padding: 8.2rem 5rem 3rem 9rem;
+    padding: 8.2rem 5rem 3rem 7rem;
+  }
+
+  @media screen and (min-width: 43em) {
+    padding: 8.2rem 8rem 3rem 7rem;
+    margin-top: -40rem;
+  }
+
+  @media screen and (min-width: 50em) {
+    padding: 8.2rem 14rem 1rem 7rem;
+  }
+
+  @media screen and (min-width: 55em) {
+    padding: 8.2rem 16rem 1rem 7rem;
+    max-width: 74%;
+  }
+
+  @media screen and (min-width: 62em) {
+    padding: 12rem 0rem 1rem 12rem;
+    place-self: end;
+    max-width: 80%;
+  }
+
+  @media screen and (min-width: 72em) {
+    max-width: 70%;
+  }
+
+  @media screen and (min-width: 82em) {
+    padding: 12rem 0rem 1rem 12rem;
+    max-width: 60%;
+  }
+
+  @media screen and (min-width: 90em) {
+    padding: 12rem 3rem 1rem 18rem;
+    max-width: 70%;
   }
 `;
 
@@ -48,6 +91,21 @@ export const AboutSpan = styled.span`
     position: absolute;
     top: -7rem;
   }
+
+  @media screen and (min-width: 62em) {
+    font-size: 20rem;
+    top: -14rem;
+  }
+
+  @media screen and (min-width: 82em) {
+    left: 0;
+  }
+
+  @media screen and (min-width: 90em) {
+    left: 6rem;
+    top: -16rem;
+    font-size: 25rem;
+  }
 `;
 
 export const HeroHeading = styled.h2`
@@ -63,6 +121,25 @@ export const HeroHeading = styled.h2`
   @media screen and (min-width: 35em) {
     font-size: 5rem;
     line-height: 5.2rem;
+  }
+
+  @media screen and (min-width: 43em) {
+    font-size: 6.4rem;
+    margin-bottom: 5rem;
+  }
+
+  @media screen and (min-width: 50em) {
+    margin-bottom: 6rem;
+  }
+
+  @media screen and (min-width: 62em) {
+    font-size: 7.2rem;
+    line-height: 7rem;
+    margin-bottom: 5rem;
+  }
+
+  @media screen and (min-width: 90em) {
+    font-size: 7.4rem;
   }
 `;
 
