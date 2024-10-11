10fc7d579e329f22b2add7ae8eac8ad7e6c6ab16
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-10-11 18:43:46 +0200

    creating the leaders data

--- app/data/index.js
+++ app/data/index.js
@@ -120,3 +120,30 @@ export const portfolioData = [
     },
   },
 ];
+
+export const leadersData = [
+  {
+    id: 1,
+    name: "Jake Richards",
+    jobTitle: "Chief Architect",
+    image: "/public/assets/about/desktop/avatar-jake.jpg",
+  },
+  {
+    id: 2,
+    name: "Thompson Smith",
+    jobTitle: "Head of Finance",
+    image: "/public/assets/about/desktop/avatar-thompson.jpg",
+  },
+  {
+    id: 3,
+    name: "Jackson Rourke",
+    jobTitle: "Lead Designer",
+    image: "/public/assets/about/desktop/avatar-jackson.jpg",
+  },
+  {
+    id: 4,
+    name: "Maria Simpson",
+    jobTitle: "Senior Architect",
+    image: "/public/assets/about/desktop/avatar-maria.jpg",
+  },
+];
