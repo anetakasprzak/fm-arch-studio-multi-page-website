6605c173bb68d2b69a538fdda01b5fed869d137e
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-10-14 18:15:18 +0100

    contact page - hero jsx

--- app/(root)/contact/page.js
+++ app/(root)/contact/page.js
@@ -1,7 +1,50 @@
+"use client";
+
 import "./page.styled";
+import {
+  ContactHeroImageWrapper,
+  ContactHeroWrapper,
+  ContactSpan,
+  HeroContactText,
+  HeroHeading,
+  HeroTextBox,
+} from "./page.styled";
 
 const ContactPage = () => {
-  return <div>Contact page</div>;
+  return (
+    <>
+      <ContactHeroWrapper>
+        <ContactHeroImageWrapper>
+          <source
+            srcSet="/assets/contact/desktop/image-hero.jpg"
+            media="(min-width: 48em)"
+          />
+          <source
+            srcSet="/assets/contact/tablet/image-hero.jpg"
+            media="(min-width: 35em)"
+          />
+          <source
+            srcSet="/assets/contact/mobile/image-hero.jpg"
+            media="(min-width: 15em)"
+          />
+          <img src="/assets/contact/mobile/image-hero.jpg" alt="hero image" />
+        </ContactHeroImageWrapper>
+
+        <HeroTextBox>
+          <ContactSpan>About</ContactSpan>
+          <HeroHeading>
+            Tell us about
+            <br /> your project
+          </HeroHeading>
+          <HeroContactText>
+            We&apos;d love to hear more about your project. Please, leave a
+            message below or give us a call. We have two offices, one in Texas
+            and one in Tennessee. If you find yourself nearby, come say hello!
+          </HeroContactText>
+        </HeroTextBox>
+      </ContactHeroWrapper>
+    </>
+  );
 };
 
 export default ContactPage;
---
+++ app/(root)/contact/page.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ContactHeroWrapper = styled.div``;
+
+export const ContactHeroImageWrapper = styled.picture``;
+
+export const HeroTextBox = styled.div``;
+
+export const ContactSpan = styled.span``;
+
+export const HeroHeading = styled.h2``;
+
+export const HeroContactText = styled.p``;
