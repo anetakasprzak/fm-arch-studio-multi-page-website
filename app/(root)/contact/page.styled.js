7f2a514348dabf28a9628f769846f6dc1a18b86e
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-10-15 13:29:16 +0100

    display hero span only on bigger screens

--- app/(root)/contact/page.styled.js
+++ app/(root)/contact/page.styled.js
@@ -4,9 +4,24 @@ export const ContactHeroWrapper = styled.div``;
 
 export const ContactHeroImageWrapper = styled.picture``;
 
-export const HeroTextBox = styled.div``;
+export const HeroTextBox = styled.div`
+  position: relative;
+`;
 
-export const ContactSpan = styled.span``;
+export const ContactSpan = styled.span`
+  display: none;
+
+  @media screen and (min-width: 35em) {
+    display: block;
+    color: #eeeff4;
+    font-size: 12rem;
+    font-weight: 700;
+    letter-spacing: -3px;
+
+    position: absolute;
+    top: -7rem;
+  }
+`;
 
 export const HeroHeading = styled.h2``;
 
